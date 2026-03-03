7cffa5219e78f64cf693564ceb65e19e633bbf87
sambain1090 <sambain1090@gmail.com>
2026-03-03 17:21:03 +1100

    added the build folder and contents

---
+++ build/CMakeFiles/Question-1-out.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/Q1/Question-1.cc" "CMakeFiles/Question-1-out.dir/Q1/Question-1.cc.o" "gcc" "CMakeFiles/Question-1-out.dir/Q1/Question-1.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281
+# Build directory: /Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(question-1 "Question-1-out")
+set_tests_properties(question-1 PROPERTIES  _BACKTRACE_TRIPLES "/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/CMakeLists.txt;13;add_test;/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/CMakeLists.txt;0;")
+add_test(question-2 "Question-2-out")
+set_tests_properties(question-2 PROPERTIES  _BACKTRACE_TRIPLES "/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/CMakeLists.txt;13;add_test;/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/CMakeLists.txt;0;")
+add_test(question-3 "Question-3-out")
+set_tests_properties(question-3 PROPERTIES  _BACKTRACE_TRIPLES "/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/CMakeLists.txt;13;add_test;/Users/sambain/Documents/redbackAssessment/av-technical-assessment-z5462281/CMakeLists.txt;0;")
